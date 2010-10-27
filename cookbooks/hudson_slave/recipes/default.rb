1742255723d1752b8e754a059ed3b6a23d1af18c
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-27 16:05:04 -0700

    use beta.3 of hudson which doesn't have activesupport 3/ruby 187 dependency

--- cookbooks/hudson_slave/recipes/default.rb
+++ cookbooks/hudson_slave/recipes/default.rb
@@ -22,13 +22,13 @@ if ['solo','app_master'].include?(node[:instance_role]) && env_name =~ /_(ci|hud
 
   gem_package "hudson" do
     source "http://gemcutter.org"
-    version "0.3.0.beta.2"
+    version "0.3.0.beta.3"
     action :install
   end
   
   execute "install_hudson_in_resin" do
-    command "/usr/local/ey_resin/ruby/bin/gem install hudson -v 0.3.0.beta.2"
-    not_if { FileTest.directory?("/usr/local/ey_resin/ruby/gems/1.8/gems/hudson-0.3.0.beta.2") }
+    command "/usr/local/ey_resin/ruby/bin/gem install hudson -v 0.3.0.beta.3"
+    not_if { FileTest.directory?("/usr/local/ey_resin/ruby/gems/1.8/gems/hudson-0.3.0.beta.3") }
   end
   
   ruby_block "add-slave-to-master" do
