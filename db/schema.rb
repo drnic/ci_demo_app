0c838e2d4be65a969a70ad1db0c26566d53a7bf8
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-26 17:36:40 -0700

    add a failing test

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20101027002750) do
+
+  create_table "posts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
---
+++ test/unit/post_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert false
+  end
+end
