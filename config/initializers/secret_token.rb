2b24a0c032b27a202129f9bf569a922d816990c4
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-26 17:26:45 -0700

    initial rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CiDemoApp::Application.config.secret_token = 'ca543ec2ac866e3a466257f1ef1b56a35d4d1cd589f6d4a52a5d590bb34233a49d90c0d825d07d9375e2933b0035b72734532c60b520dbda22b0abcb51b355da'
