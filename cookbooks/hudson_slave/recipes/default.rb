9dbd6c443a627add088fc5238acc736eed5641d9
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-28 15:54:53 -0700

    creating git config required by git plugin

--- cookbooks/hudson_slave/recipes/default.rb
+++ cookbooks/hudson_slave/recipes/default.rb
@@ -13,6 +13,7 @@
 # Config - move to attributes
 
 env_name = node[:environment][:name]
+username = node[:users].first[:username]
 
 if ['solo','app_master'].include?(node[:instance_role]) && env_name =~ /_(ci|hudson_slave)$/
   # gem_package "hudson" do
@@ -24,6 +25,22 @@ if ['solo','app_master'].include?(node[:instance_role]) && env_name =~ /_(ci|hud
   execute "install_hudson_in_resin" do
     command "/usr/local/ey_resin/ruby/bin/gem install #{node[:hudson][:gem][:install]}"
     not_if { FileTest.directory?("/usr/local/ey_resin/ruby/gems/1.8/gems/#{node[:hudson][:gem][:version]}") }
+  end
+  
+  ruby_block "authorize_hudson_master_key" do
+    authorized_keys = "/home/#{node[:users].first[:username]}/.ssh/authorized_keys"
+    block do
+      File.open(authorized_keys, "a") do |f|
+        f.puts node[:hudson][:master][:public_key]
+      end
+    end
+    not_if "grep '#{node[:hudson][:master][:public_key]}' #{authorized_keys}"
+  end
+  
+  # TODO
+  execute "setup-git-config-for-tagging" do
+    command %Q{ sudo su #{username} -c "git config --global user.email 'you@example.com' && git config --global user.name 'You are Special'" }
+    not_if  %Q{ sudo su #{username} -c "git config user.email" }
   end
   
   ruby_block "add-slave-to-master" do
@@ -39,7 +56,7 @@ if ['solo','app_master'].include?(node[:instance_role]) && env_name =~ /_(ci|hud
         :name        => env_name,
         :description => "Automatically added by Engine Yard AppCloud for environment #{env_name}",
         :slave_host  => node[:engineyard][:environment][:instances].first[:public_hostname],
-        :slave_user  => node[:users].first[:username],
+        :slave_user  => username,
         :executors   => [node[:applications].size, 1].max,
         :label       => node[:applications].keys.join(" ")
       )
