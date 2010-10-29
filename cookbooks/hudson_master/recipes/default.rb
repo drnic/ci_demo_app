01875ff0527dbfc6a03f270f51a71f20b45e2cad
Dr Nic Williams <drnicwilliams@gmail.com>
2010-10-29 14:42:32 -0700

    only allow hudson_master to be run on a 'solo' instance; for the moment

--- cookbooks/hudson_master/recipes/default.rb
+++ cookbooks/hudson_master/recipes/default.rb
@@ -18,73 +18,76 @@ hudson_home = "/data/hudson-ci"
 hudson_pid  = "#{hudson_home}/tmp/pid"
 plugins     = node[:hudson_master][:plugins]
 
-execute "setup-git-config-for-tagging" do
-  command %Q{ sudo su #{username} -c "git config --global user.email 'you@example.com' && git config --global user.name 'You are Special'" }
-  not_if  %Q{ sudo su #{username} -c "git config user.email" }
-end
+if ['solo'].include?(node[:instance_role])
 
-%w[logs tmp war plugins .].each do |dir|
-  directory "#{hudson_home}/#{dir}" do
-    owner hudson_user
-    group hudson_user
-    mode  0755 unless dir == "war"
-    action :create
-    recursive true
-  end
-end
-
-remote_file "#{hudson_home}/hudson.war" do
-  source "http://hudson-ci.org/latest/hudson.war"
-  owner hudson_user
-  group hudson_user
-  not_if { FileTest.exists?("#{hudson_home}/hudson.war") }
-end
-
-template "/etc/init.d/hudson" do
-  source "init.sh.erb"
-  owner "root"
-  group "root"
-  mode 0755
-  variables(
-    :user => hudson_user,
-    :port => hudson_port,
-    :home => hudson_home,
-    :pid  => hudson_pid
-  )
-  not_if { FileTest.exists?("/etc/init.d/hudson") }
-end
-
-plugins.each do |plugin|
-  remote_file "#{hudson_home}/plugins/#{plugin}.hpi" do
-    source "http://hudson-ci.org/latest/#{plugin}.hpi"
-    owner hudson_user
-    group hudson_user
-    not_if { FileTest.exists?("#{hudson_home}/plugins/#{plugin}.hpi") }
+  execute "setup-git-config-for-tagging" do
+    command %Q{ sudo su #{username} -c "git config --global user.email 'you@example.com' && git config --global user.name 'You are Special'" }
+    not_if  %Q{ sudo su #{username} -c "git config user.email" }
   end
 
-end
+  %w[logs tmp war plugins .].each do |dir|
+    directory "#{hudson_home}/#{dir}" do
+      owner hudson_user
+      group hudson_user
+      mode  0755 unless dir == "war"
+      action :create
+      recursive true
+    end
+  end
 
-template "/data/nginx/servers/hudson_reverse_proxy.conf" do
-  source "proxy.conf.erb"
-  owner hudson_user
-  group hudson_user
-  mode 0644
-  variables(
-    :port => hudson_port
-  )
-  not_if { FileTest.exists?("/data/nginx/servers/hudson_reverse_proxy.conf") }
-end
+  remote_file "#{hudson_home}/hudson.war" do
+    source "http://hudson-ci.org/latest/hudson.war"
+    owner hudson_user
+    group hudson_user
+    not_if { FileTest.exists?("#{hudson_home}/hudson.war") }
+  end
 
-execute "ensure-hudson-is-running" do
-  command "/etc/init.d/hudson restart"
-end
+  template "/etc/init.d/hudson" do
+    source "init.sh.erb"
+    owner "root"
+    group "root"
+    mode 0755
+    variables(
+      :user => hudson_user,
+      :port => hudson_port,
+      :home => hudson_home,
+      :pid  => hudson_pid
+    )
+    not_if { FileTest.exists?("/etc/init.d/hudson") }
+  end
 
-execute "Restart nginx" do
-  command "/etc/init.d/nginx restart"
-end
+  plugins.each do |plugin|
+    remote_file "#{hudson_home}/plugins/#{plugin}.hpi" do
+      source "http://hudson-ci.org/latest/#{plugin}.hpi"
+      owner hudson_user
+      group hudson_user
+      not_if { FileTest.exists?("#{hudson_home}/plugins/#{plugin}.hpi") }
+    end
 
-execute "Generate key pair for slaves" do
-  key_path = "/home/#{node[:users].first[:username]}/.ssh/id_rsa"
-  command "ssh-keygen -f #{key_path} -N ''"
-  not_if { FileTest.exists?(key_path) }
+  end
+
+  template "/data/nginx/servers/hudson_reverse_proxy.conf" do
+    source "proxy.conf.erb"
+    owner hudson_user
+    group hudson_user
+    mode 0644
+    variables(
+      :port => hudson_port
+    )
+    not_if { FileTest.exists?("/data/nginx/servers/hudson_reverse_proxy.conf") }
+  end
+
+  execute "ensure-hudson-is-running" do
+    command "/etc/init.d/hudson restart"
+  end
+
+  execute "Restart nginx" do
+    command "/etc/init.d/nginx restart"
+  end
+
+  execute "Generate key pair for slaves" do
+    key_path = "/home/#{node[:users].first[:username]}/.ssh/id_rsa"
+    command "ssh-keygen -f #{key_path} -N ''"
+    not_if { FileTest.exists?(key_path) }
+  end
 end
