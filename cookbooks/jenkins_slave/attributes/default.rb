0fb28d60ef1f4e1575b4404d5103b018d2b9c12f
Dr Nic Williams <drnicwilliams@gmail.com>
2011-04-13 18:23:45 -0700

    Remove whitespace in pubkey

--- cookbooks/jenkins_slave/attributes/default.rb
+++ cookbooks/jenkins_slave/attributes/default.rb
@@ -7,7 +7,7 @@ jenkins_slave({
   :master => {
     :host => "mysecretapp.com",
     :port => 80,
-    :public_key => "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtz9dQqsUsUCf1vkk3krI4b2Dxaaiv49/p9/X1xYWYj5rhvRzvBalmBmXydj37ND6wf370b6kYqiUbFY3Dn+gCK5EIBlMrreRYE+tGHwAKKDwOrAkMtLBYtKseTs+SVSWyJwobOiMsyIEyAU1g2cbBwd3MBDcuBb3y9piulVcZevHdnKztgNWoz2oxWemnlQsHcVL31VgnPZQURz9U1MlLn/QAjtRW9xOlXCiTYVEh63svTsWmiPrDKuXAnhicD88jETUnu44WRGcY/ygmlX9b1/KOHm22Vw7jUL+Q9+J+nUXCYXG7pf6t8Ul2NYF+iAoFzCF+cLbmKB6QshcDr6ZLQ== root@ip-10-66-101-144\n",
+    :public_key => "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAtz9dQqsUsUCf1vkk3krI4b2Dxaaiv49/p9/X1xYWYj5rhvRzvBalmBmXydj37ND6wf370b6kYqiUbFY3Dn+gCK5EIBlMrreRYE+tGHwAKKDwOrAkMtLBYtKseTs+SVSWyJwobOiMsyIEyAU1g2cbBwd3MBDcuBb3y9piulVcZevHdnKztgNWoz2oxWemnlQsHcVL31VgnPZQURz9U1MlLn/QAjtRW9xOlXCiTYVEh63svTsWmiPrDKuXAnhicD88jETUnu44WRGcY/ygmlX9b1/KOHm22Vw7jUL+Q9+J+nUXCYXG7pf6t8Ul2NYF+iAoFzCF+cLbmKB6QshcDr6ZLQ== root@ip-10-66-101-144",
     :master_key_location => "/home/deploy/.ssh/id_rsa"
   },
   :gem => {
